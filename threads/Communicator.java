b2ace5ebfdb92e6f03ee398a3f0f23fd3060986f
O-Ayala <olmospsnemail1@gmail.com>
2021-10-15 16:48:59 -0700

    Implemented CommunicatorTest.java

--- threads/Communicator.java
+++ threads/Communicator.java
@@ -68,6 +68,135 @@ public class Communicator {
 		return message;
 	}
 
+	// Add communicator testing code to the Communicator class
+
+
+/**
+	 * Test with 1 listener then 1 speaker.
+	 */
+	public static void selfTest1() {
+
+		KThread listener1 = new KThread(listenRun);
+		listener1.setName("listener1");
+		listener1.fork();
+
+		KThread speaker1 = new KThread(speakerRun);
+		speaker1.setName("speaker1");
+		speaker1.fork();
+
+	} // selfTest1()
+
+	/**
+	 * Test with 1 speaker then 1 listener.
+	 */
+	public static void selfTest2() {
+
+		KThread speaker1 = new KThread(speakerRun);
+		speaker1.setName("speaker1");
+		speaker1.fork();
+
+		KThread listener1 = new KThread(listenRun);
+		listener1.setName("listener1");
+		listener1.fork();
+
+	} // selfTest2()
+
+	/**
+	 * Test with 2 speakers and 2 listeners intermixed.
+	 */
+	public static void selfTest3() {
+
+		KThread speaker1 = new KThread(speakerRun);
+		speaker1.setName("speaker1");
+		speaker1.fork();
+
+		KThread listener1 = new KThread(listenRun);
+		listener1.setName("listener1");
+		listener1.fork();
+
+		KThread speaker2 = new KThread(speakerRun);
+		speaker2.setName("speaker2");
+		speaker2.fork();
+
+		KThread listener2 = new KThread(listenRun);
+		listener2.setName("listener2");
+		listener2.fork();
+
+	} // selfTest3()
+
+	/**
+	 * Function to run inside Runnable object listenRun. Uses the function listen on
+	 * static object myComm inside this class, allowing the threads inside the
+	 * respective selfTests above to call the runnable variables below and test
+	 * functionality for listen. Needs to run with debug flags enabled. See NACHOS
+	 * README for info on how to run in debug mode.
+	 */
+	static void listenFunction() {
+		Lib.debug(dbgThread, "Thread " + KThread.currentThread().getName() + " is about to listen");
+
+		Lib.debug(dbgThread, "Thread " + KThread.currentThread().getName() + " got value " + myComm.listen());
+
+	} // listenFunction()
+
+	/**
+	 * Function to run inside Runnable object speakerRun. Uses the function listen
+	 * on static object myComm inside this class, allowing the threads inside the
+	 * respective selfTests above to call the runnable variables below and test
+	 * functionality for speak. Needs to run with debug flags enabled. See NACHOS
+	 * README for info on how to run in debug mode.
+	 */
+	static void speakFunction() {
+		Lib.debug(dbgThread, "Thread " + KThread.currentThread().getName() + " is about to speak");
+
+		myComm.speak(myWordCount++);
+
+		Lib.debug(dbgThread, "Thread " + KThread.currentThread().getName() + " has spoken");
+	} // speakFunction()
+
+	/**
+	 * Wraps listenFunction inside a Runnable object so threads can be generated for
+	 * testing.
+	 */
+	private static Runnable listenRun = new Runnable() {
+		public void run() {
+			listenFunction();
+		}
+	}; // runnable listenRun
+
+	/**
+	 * Wraps speakFunction inside a Runnable object so threads can be generated for
+	 * testing.
+	 */
+	private static Runnable speakerRun = new Runnable() {
+		public void run() {
+			speakFunction();
+		}
+	}; // Runnable speakerRun
+
+
+
+	// Implement more test methods here ...
+
+
+    // Invoke Communicator.selfTest() from ThreadedKernel.selfTest()
+
+	public static void selfTest() {
+	selfTest1();
+	selfTest2();
+	selfTest3();
+
+
+	// Invoke your other test methods here ...
+
+}
+
+	// dbgThread = 't' variable needed for debug output
+	private static final char dbgThread = 't';
+	// myComm is a shared object that tests Communicator functionality
+	private static Communicator myComm = new Communicator();
+	// myWordCount is used for selfTest5 when spawning listening/speaking threads
+	private static int myWordCount = 0;
+
 	private Lock lock = new Lock();
 
 	private Condition canListen = new Condition(lock);
