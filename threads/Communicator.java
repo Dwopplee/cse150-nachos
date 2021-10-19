ccde136c5b912e9756cb635ffb5d7ea244585d12
Dwopplee <charlesparkinson14@gmail.com>
2021-10-19 13:55:50 -0700

    Added some documentation

--- threads/Communicator.java
+++ threads/Communicator.java
@@ -27,18 +27,24 @@ public class Communicator {
 	 * @param word the integer to transfer.
 	 */
 	public void speak(int word) {
+		// Ensure atomicity
 		lock.acquire();
 
+		// Prevent this speaker from overwriting a previous speaker's word
 		while (words) {
 			canSpeak.sleep();
 		}
 
+		// Once the buffer is empty, add our word to it, then flag it as full
 		this.word = word;
-
 		words = true;
 
+		// Wake any waiting listeners
 		canListen.wake();
+		// Go to sleep to ensure we are partnered before we return
 		canSpeak.sleep();
+
+		// Wake the next waiting speaker
 		canSpeak.wake();
 
 		lock.release();
@@ -51,17 +57,24 @@ public class Communicator {
 	 * @return the integer transferred.
 	 */
 	public int listen() {
+		// Ensure atomicity
 		lock.acquire();
 
+		// Prevent the listener from accessing an empty buffer
 		while (!words) {
 			canListen.sleep();
 		}
 
+		// Once the buffer is full, take the word and flag it as empty
 		int message = word;
-
 		words = false;
 
+		// Notify the speaker it is partnered
+		// This message may be intercepted by another waiting speaker
+		// That's okay -- the speaker will pass it along until it reaches the right one
 		canSpeak.wake();
+
+		// We don't have to sleep here -- if we have a word we have a partner
 
 		lock.release();
 
@@ -197,7 +210,9 @@ public class Communicator {
 	private Condition canListen = new Condition(lock);
 	private Condition canSpeak = new Condition(lock);
 
+	// Flag to indicate if the buffer is full
 	private boolean words = false;
 
+	// Buffer with which to pass messages between speak() and listen()
 	private int word;
 }
